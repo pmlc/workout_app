9a130eb25c16784b1e4410cd487aef6d8817b682
“pmlc” <pierre.lecount@gmail.com>
2016-02-12 05:45:54 +0000

    Rspec and signup added

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -2,20 +2,16 @@ require 'rails_helper'
 
 RSpec.feature "Users sign up" do
   
-  before do
-    @john = User.create!(email: "john@example.com", password: "password")
-  end
-  
   scenario "with valid credentials" do
     visit "/"
       
-    click_link "Sign in"
-    fill_in "Email", with: @john.email
-    fill_in "Password", with: @john.password
-    click_button "Log in"
+    click_link "Sign up"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
     
-    expect(page).to have_content("Signed in successfully.")
-    expect(page).to have_content("Signed in as #{@john.email}")
+    expect(page).to have_content("You have signed up successfully.")
   end
   
  end
