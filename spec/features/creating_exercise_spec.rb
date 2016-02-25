2ecccb3d533de9700b8f8d590306255058f4e81a
“pmlc” <pierre.lecount@gmail.com>
2016-02-25 21:23:05 +0000

    Confirm exercises validation of exercises

--- spec/features/creating_exercise_spec.rb
+++ spec/features/creating_exercise_spec.rb
@@ -11,6 +11,28 @@ RSpec.feature "Creating Exercise" do
     login_as(@john)
   end
   
+  scenario "with invalid inputs" do
+    visit "/"
+    
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
+    
+    fill_in "Duration", with: nil
+    fill_in "Workout Details", with: ""
+    fill_in "Activity date", with: ""
+    click_button "Create Exercise"
+    
+    expect(page).to have_content(" errors prohibited this exercise from being saved:")
+    
+    expect(page).to have_content("Duration in min can't be blank")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout can't be blank")
+    expect(page).to have_content("Workout date can't be blank")
+  
+  end
+  
+  
   scenario "with valid inputs" do
     visit "/"
     
@@ -28,5 +50,4 @@ RSpec.feature "Creating Exercise" do
     exercise = Exercise.last
     expect(page.current_path).to eq(user_exercise_path(@john, exercise))
   end
-  
 end
