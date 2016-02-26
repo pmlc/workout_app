f58569433fdcab3bc588c521d84784f1626ddc3e
“pmlc” <pierre.lecount@gmail.com>
2016-02-26 04:57:35 +0000

    Exercises - added to index listing

---
+++ spec/features/listing_exercises_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "Listing Exercise" do
+  
+  before do
+    @john = User.create(first_name: "John",
+                        last_name: "Doe",
+                        email: "john@example.com", 
+                        password: "password")
+    login_as(@john)
+    
+    @e1 = @john.exercises.create(duration_in_min: 10, workout: "gymnastics", workout_date: "2016-02-02")
+    @e2 = @john.exercises.create(duration_in_min: 20, workout: "stretching", workout_date: "2016-02-03")
+  end
+  
+  scenario "shows users workout for last 7 days" do
+    visit "/"
+    
+    click_link "My Lounge"
+    expect(page).to have_content(@e1.duration_in_min)
+    expect(page).to have_content(@e1.workout)
+    expect(page).to have_content(@e1.workout_date)
+    expect(page).to have_content(@e2.duration_in_min)
+    expect(page).to have_content(@e2.workout)
+    expect(page).to have_content(@e2.workout_date)
+    
+  end
+end
